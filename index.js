94dc5ec6008b6117fc4a6bde6c55a8fe4bc1b621
Toby Rahilly <toby@mobikontech.com>
2014-08-21 10:32:26 +0800

    Better support for different content formats. Now supports raw loader as well as html loader.

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ var loaderUtils = require("loader-utils");
 
 module.exports = function (content) {
     this.cacheable && this.cacheable();
+
     var query = loaderUtils.parseQuery(this.query);
     var ngModule = query.module || 'ngTemplates';
     var relativeTo = query.relativeTo || '';
     var path = this.resource.slice(relativeTo.length); // get the base path
-    var html = content.slice(17, -1); // trim the module.exports = prefix and ; postfix
+    var html = content.match(/^module\.exports/) ?
+        content.substr(findQuote(content, false), findQuote(content, true)) :
+        content;
 
-    return "angular.module('" + ngModule + "').run(['$templateCache', function(c) { c.put('"+ path +"', " + html + ") }]);"
+    return "angular.module('" + ngModule + "').run(['$templateCache', function(c) { c.put('"+ path +"', " + html + ") }]);";
+
+    function findQuote(content, backwards) {
+        var i = backwards ? content.length - 1 : 0;
+        while (i >= 0 && i < content.length) {
+            if (content[i] == '"' || content[i] == "'") {
+                return i;
+            }
+            i += backwards ? -1 : 1;
+        }
+        return -1;
+    }
 };
