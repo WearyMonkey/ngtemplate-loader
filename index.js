0360c22842d6bb966077e2ddca0f287a68f6bc3c
Andreas Streichardt <andreas.streichardt@gmail.com>
2015-10-19 16:29:26 +0200

    Implement proper chained loading using module.exports instead of string magic

--- index.js
+++ index.js
@@ -38,31 +38,11 @@ module.exports = function (content) {
     }
 
     var filePath = prefix + resource.slice(relativeToIndex + relativeTo.length); // get the base path
-    var html;
 
-    if (content.match(/^module\.exports/)) {
-        var firstQuote = findQuote(content, false);
-        var secondQuote = findQuote(content, true);
-        html = content.substr(firstQuote, secondQuote - firstQuote + 1);
-    } else {
-        html = content;
-    }
-
-    return "var path = '"+jsesc(filePath)+"';\n" +
-        "var html = " + html + ";\n" +
+    return content + "\n\nvar path = '"+jsesc(filePath)+"';\n" +
+        "var html = module.exports;\n" +
         "window.angular.module('" + ngModule + "').run(['$templateCache', function(c) { c.put(path, html) }]);\n" +
         "module.exports = path;";
-
-    function findQuote(content, backwards) {
-        var i = backwards ? content.length - 1 : 0;
-        while (i >= 0 && i < content.length) {
-            if (content[i] == '"' || content[i] == "'") {
-                return i;
-            }
-            i += backwards ? -1 : 1;
-        }
-        return -1;
-    }
 
     // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
     function escapeRegExp(string) {
